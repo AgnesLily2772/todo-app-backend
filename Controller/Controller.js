ac4073514f50aea64bb95e9449cf5268b9044d6c
AGNES LILY <agneslily2772@gmail.com>
2023-11-27 23:04:25 +0530

    Update Controller.js

--- Controller/Controller.js
+++ Controller/Controller.js
@@ -32,9 +32,9 @@ export const signInUser = async (req, res) => {
                 
                 const token = await user.generateSessionToken();
                 //production
-                //res.cookie("jwtoken", token, { httpOnly: true, expires: new Date(Date.now() + 3600000), secure: true, sameSite:'none' })
+                res.cookie("jwtoken", token, { httpOnly: true, expires: new Date(Date.now() + 3600000), secure: true, sameSite:'none' })
                 //development
-                res.cookie("jwtoken", token, { httpOnly: false, expires: new Date(Date.now() + 3600000), secure: true, sameSite:'none' })
+                //res.cookie("jwtoken", token, { httpOnly: false, expires: new Date(Date.now() + 3600000), secure: true, sameSite:'none' })
                 res.status(200).json(user);
         } 
         catch (error) {
@@ -46,8 +46,9 @@ export const getUserData = (req, res) =>{
         return res.send(req.rootUser);
 }
 
-export const signOutUser = async(req, res)=> {
-        await res.clearCookie("jwtoken",{path:'/'});
+export const signOutUser = (req, res)=> {
+        res.cookie('jwtoken', '', httpOnly: true, expires: new Date(0)});
+        //res.clearCookie("jwtoken",{path:'/'});
         res.status(200).send(`userLogout`);
 }
 
